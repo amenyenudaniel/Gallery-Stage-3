e87ff5a91f8b9f9bb7f6e73e4873e5146cebb314
amenyenudaniel <amenyenudaniel321@gmail.com>
2023-09-19 14:36:58 +0000

    footer is done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,28 @@
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import { footerLinks } from "../../utils/data";
 import "./Footer.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="footer__links">
+        {footerLinks.map((item) => (
+          <div key={item.id}>
+            {item.links.map((link) => (
+              <p className="linkName">{link.linkName}</p>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      <div className="social__icons">
+        <FaFacebook />
+        <FaInstagram />
+        <FaTwitter />
+        <FaYoutube />
+      </div>
+      <p className="copyright">&copy; 2023 AIGallery. All Rights Reserved.</p>
+    </footer>
+  );
 };
 
 export default Footer;
