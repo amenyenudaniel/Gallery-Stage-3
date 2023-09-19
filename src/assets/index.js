c61c1b0b843d371c0196d9bd52b0864f1c2c640a
amenyenudaniel <amenyenudaniel321@gmail.com>
2023-09-19 13:17:08 +0000

    Hero and Welcome Section is done

---
+++ src/assets/index.js
@@ -0,0 +1,31 @@
+import camera from "./camera.svg";
+import gallery1 from "./gallery1.jpg";
+import gallery2 from "./gallery2.jpeg";
+import gallery3 from "./gallery3.jpg";
+import gallery4 from "./gallery4.jpg";
+import gallery5 from "./gallery5.jpg";
+import gallery6 from "./gallery6.jpg";
+import gallery7 from "./gallery7.jpg";
+import gallery8 from "./gallery8.jpeg";
+import star1 from "./star1.svg";
+import star2 from "./star2.svg";
+import star3 from "./star3.svg";
+import star4 from "./star4.svg";
+import star5 from "./star5.svg";
+
+export {
+  camera,
+  gallery1,
+  gallery2,
+  gallery3,
+  gallery4,
+  gallery5,
+  gallery6,
+  gallery7,
+  gallery8,
+  star1,
+  star2,
+  star3,
+  star4,
+  star5,
+};
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,6 +1,29 @@
 import "./Welcome.css";
+import { camera, star1, star2, star3, star4, star5 } from "../../assets";
 const Welcome = () => {
-  return <section className="welcome__section"></section>;
+  return (
+    <section className="welcome__section">
+      <div className="welcome__banner">
+        <div className="welcome__content">
+          <h2>Welcome to the AIGallery</h2>
+          <p>
+            Dive into an ocean of artistic wonders and capture your senses with
+            our breathtaking art and photography collections! Are you ready to
+            embrace the extraordinary?
+          </p>
+        </div>
+        <img src={camera} alt="camera__image" />
+      </div>
+
+      <div className="welcome__stars">
+        <img src={star1} alt="star__image" className="first__image" />
+        <img src={star2} alt="star__image" className="last__image" />
+        <img src={star3} alt="star__image" className="middle__image" />
+        <img src={star4} alt="star__image" className="last__image" />
+        <img src={star5} alt="star__image" className="first__image" />
+      </div>
+    </section>
+  );
 };
 
 export default Welcome;
